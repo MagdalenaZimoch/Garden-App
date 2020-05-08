f9baa8a24a5b0d2262eea948c81e5f221a7400d5
Magdalena Zimoch <36634546+MagdalenaZimoch@users.noreply.github.com>
2020-05-08 23:21:26 +0200

    Dump poprawiony z SQLite

--- Model fizyczny i konceptualny/Aplikacja-GARDEN.sql
+++ Model fizyczny i konceptualny/Aplikacja-GARDEN.sql
@@ -80,57 +80,63 @@ INSERT INTO "egzemplarze" ("id_e","id_ogrodka","id_rosliny","miejsce","status","
  (10,1,1,'A3','NZ','2020-05-03 19:11:47'),
  (15,1,1,'A7','NZ','2020-05-03 21:10:33'),
  (17,1,3,'A8','NZ','2020-05-04 13:52:57'),
- (18,1,1,'a10','NZ','2020-05-05 12:10:58'),
- (19,1,1,'A32','NZ','2020-05-08 17:09:30');
-INSERT INTO "powiadomienia" ("id_p","id_egzemplarza","id_ogrodka","tresc","data_powiadomienia") VALUES (2,NULL,1,' Przekop ogrÃ³dek','2020-03-01 12:00:00'),
- (54,1,1,'Nie zapomnij mnie zasadziÄ‡!','2020-04-15 00:00:00'),
- (55,1,1,'MoÅ¼esz mnie zasadziÄ‡!','2020-03-15 00:00:00'),
- (56,2,1,'Nie zapomnij mnie zasadziÄ‡!','2020-05-30 00:00:00'),
- (57,2,1,'MoÅ¼esz mnie zasadziÄ‡!','2020-03-01 00:00:00'),
- (58,3,2,'Nie zapomnij mnie zasadziÄ‡!','2020-04-15 00:00:00'),
- (59,3,2,'MoÅ¼esz mnie zasadziÄ‡!','2020-03-15 00:00:00'),
- (64,4,2,'Nie zapomnij mnie zasadziÄ‡!','2020-05-30 00:00:00'),
- (65,4,2,'MoÅ¼esz mnie zasadziÄ‡!','2020-03-01 00:00:00'),
- (70,8,2,'Nie zapomnij mnie zasadziÄ‡!','2020-05-30 00:00:00'),
- (71,8,2,'MoÅ¼esz mnie zasadziÄ‡!','2020-03-01 00:00:00'),
- (72,10,1,'Nie zapomnij mnie zasadziÄ‡!','2020-04-15 00:00:00'),
- (73,10,1,'MoÅ¼esz mnie zasadziÄ‡!','2020-03-15 00:00:00'),
- (74,15,1,'Nie zapomnij mnie zasadziÄ‡!','2020-04-15 00:00:00'),
- (75,15,1,'MoÅ¼esz mnie zasadziÄ‡!','2020-03-15 00:00:00'),
- (76,17,1,'Nie zapomnij mnie zasadziÄ‡!','2020-04-15 00:00:00'),
- (77,17,1,'MoÅ¼esz mnie zasadziÄ‡!','2020-03-15 00:00:00'),
- (78,18,1,'Nie zapomnij mnie zasadziÄ‡!','2020-04-15 00:00:00'),
- (79,18,1,'MoÅ¼esz mnie zasadziÄ‡!','2020-03-15 00:00:00'),
- (80,19,1,'Nie zapomnij mnie zasadzić!','2020-04-15 00:00:00'),
- (81,NULL,13,'Przekop ogródek!','2021-03-01 00:00:00'),
- (82,NULL,14,'Przekop ogródek!','2021-04-01 00:00:00');
-INSERT INTO "obserwacje" ("id_o","id_uzytkownika","id_egzemplarza","tresc","data_obserwacji") VALUES (1,1,1,'Marchew roÅ›nie ksiÄ…Å¼kowo','2020-05-03 12:05:48'),
- (2,1,1,'Faza wzrostu: poczÄ…tkowa ','2020-05-03 12:24:31'),
- (9,2,3,'faza wzrostu: poczÄ…tkowa','2020-05-03 16:45:18'),
- (12,1,10,'Faza wzrostu: poczÄ…tkowa ','2020-05-05 13:26:27'),
- (19,1,17,'Faza wzrostu: poczÄ…tkowa ','2020-05-05 13:27:46'),
- (20,1,18,'Faza wzrostu: poczÄ…tkowa ','2020-05-05 13:27:51');
-INSERT INTO "ogrodki" ("id_og","id_uzytkownika","nazwa","adres") VALUES (1,1,'OgrÃ³dek warzywny','ul. Kolejowa 22 BorkÃ³w'),
- (2,2,'OgrÃ³dek1','ul. Majowa 13 Malczyce'),
- (3,1,'OgrÃ³dek owocowy','ul.Kolejowa 22 BorkÃ³w'),
- (5,5,'OgrÃ³dek A4','ul. Brzoskwiniowa 6 WrocÅ‚aw'),
- (8,1,'Ogrodek mamy','ul. WrocÅ‚awska 2 WrocÅ‚aw'),
- (9,1,'Ogródek warzywny - nowy','ul. Kolejowa 22 Borków'),
- (10,1,'Ogródek warzywny - nowy NOWY nowy','ul. Kolejowa 22 Borków'),
- (11,1,'Ogródek warzywny - nowy NOWY nowy Nowy','ul. Kolejowa 22 Borków'),
- (12,1,'Ogródek warzywny - nowy NOWY nowy Nowy nowyyy','ul. Kolejowa 22 Borków'),
- (13,1,'Ogródek warzywny - nowy NOWY nowy Nowy nowyyy nowyyyyy','ul. Kolejowa 22 Borków'),
+ (22,1,3,'A21','ZE','2020-05-08 20:59:24');
+INSERT INTO "powiadomienia" ("id_p","id_egzemplarza","id_ogrodka","tresc","data_powiadomienia") VALUES (2,NULL,1,' Przekop ogródek','2020-03-01 12:00:00'),
+ (82,NULL,14,'Przekop ogródek!','2021-04-01 00:00:00'),
+ (90,1,1,'Nie zapomnij mnie zasadzić!','2020-04-15 00:00:00'),
+ (91,1,1,'Możesz mnie zasadzić!','2020-03-15 00:00:00'),
+ (92,2,1,'Nie zapomnij mnie zasadzić!','2020-05-30 00:00:00'),
+ (93,2,1,'Możesz mnie zasadzić!','2020-03-01 00:00:00'),
+ (94,3,2,'Nie zapomnij mnie zasadzić!','2020-04-15 00:00:00'),
+ (95,3,2,'Możesz mnie zasadzić!','2020-03-15 00:00:00'),
+ (96,4,2,'Nie zapomnij mnie zasadzić!','2020-05-30 00:00:00'),
+ (97,4,2,'Możesz mnie zasadzić!','2020-03-01 00:00:00'),
+ (98,8,2,'Nie zapomnij mnie zasadzić!','2020-05-30 00:00:00'),
+ (99,8,2,'Możesz mnie zasadzić!','2020-03-01 00:00:00'),
+ (100,10,1,'Nie zapomnij mnie zasadzić!','2020-04-15 00:00:00'),
+ (101,10,1,'Możesz mnie zasadzić!','2020-03-15 00:00:00'),
+ (102,15,1,'Nie zapomnij mnie zasadzić!','2020-04-15 00:00:00'),
+ (103,15,1,'Możesz mnie zasadzić!','2020-03-15 00:00:00'),
+ (104,17,1,'Nie zapomnij mnie zasadzić!','2020-04-15 00:00:00'),
+ (105,17,1,'Możesz mnie zasadzić!','2020-03-15 00:00:00');
+INSERT INTO "obserwacje" ("id_o","id_uzytkownika","id_egzemplarza","tresc","data_obserwacji") VALUES (1,1,1,'Marchew rośnie książkowo.','2020-05-03 12:05:48'),
+ (2,1,1,'Faza wzrostu: początkowa ','2020-05-03 12:24:31'),
+ (9,2,3,'faza wzrostu: początkowa','2020-05-03 16:45:18'),
+ (12,1,10,'Faza wzrostu: początkowa ','2020-05-05 13:26:27'),
+ (19,1,17,'Faza wzrostu: początkowa ','2020-05-05 13:27:46');
+INSERT INTO "ogrodki" ("id_og","id_uzytkownika","nazwa","adres") VALUES (0,1,'Ogródek warzywny - nowy','ul. Kolejowa 22 Borków'),
+ (1,1,'Ogródek warzywny','ul. Kolejowa 22 Borków'),
+ (2,2,'Ogródek1','ul. Majowa 13 Malczyce'),
+ (3,1,'Ogródek owocowy','ul.Kolejowa 22 Borków'),
+ (5,5,'Ogródek A4','ul. Brzoskwiniowa 6 Wrocław'),
+ (8,1,'Ogrodek mamy','ul. Wrocławska 2 Wrocław
+'),
  (14,1,'Ogródek warzywny - nowy NOWY nowy Nowy nowyyy nowyyyyy 2','ul. Kolejowa 22 Borków');
-INSERT INTO "zasady" ("id_z","id_rosliny","tresc") VALUES (1,5,'Stanowisko: sÅ‚oneczneâ€šcieniste, z podÅ‚oÅ¼em przepuszczalnym, prÃ³chnicznym i nieprzesychajÄ…cym'),
- (2,5,'gatunek nie jest bowiem odporny na spadki temperatury poniÅ¼ej 0Â°C'),
- (3,3,'Stanowisko: Burak lubi rosnÄ…Ä‡ w miejscu z dostÄ™pem do Å›wiatÅ‚a, chociaÅ¼ stanowisko pod uprawÄ™ tych warzyw nie powinno znajdowaÄ‡ siÄ™ w peÅ‚nym sÅ‚oÅ„cu. Jest to roÅ›lina, ktÃ³ra najlepiej czuje siÄ™ na glebie wilgotnej i lekkiej. Optymalny dla jego rozwoju jest obojÄ™tny odczyn pH podÅ‚oÅ¼a. Do uprawy buraka Opolskiego w miarÄ™ moÅ¼liwoÅ›ci wybieramy mniej kamieniste miejsca.'),
- (4,3,'Rozstawa: Nasiona buraka Opolskiego wysiewamy w rzÄ™dach oddalonych o 30-40 centymetrÃ³w. Po wzejÅ›ciu roÅ›lin robimy przerywkÄ™ pozostawiajÄ…c roÅ›liny co okoÅ‚o 5 centymetrÃ³w.'),
- (5,2,'Warunki uprawy: pietruszkÄ™ naciowÄ… naleÅ¼y uprawiaÄ‡ na glebach lekkich, obojÄ™tnych i bogatych w prÃ³chnicÄ™. NaleÅ¼y unikaÄ‡ gleb podmokÅ‚ych, kamienistych i zaskorupiajÄ…cych siÄ™.');
+INSERT INTO "zasady" ("id_z","id_rosliny","tresc") VALUES (1,5,'Stanowisko: słoneczne, z podłożem przepuszczalnym, próchnicznym i nieprzesychającym'),
+ (2,5,'gatunek nie jest bowiem odporny na spadki temperatury poniżej 0°C'),
+ (3,3,'Stanowisko: Burak lubi rosnący w miejscu z dostępem do światła, chociaż stanowisko pod uprawę tych warzyw nie powinno znajdować się w pełnym słońcu. Jest to roślina, która najlepiej czuje się na glebie wilgotnej i lekkiej. Optymalny dla jego rozwoju jest obojętny odczyn pH podłoża. Do uprawy buraka Opolskiego w miarę możliwości wybieramy mniej kamieniste miejsca.'),
+ (4,3,'Rozstawa: Nasiona buraka Opolskiego wysiewamy w rzędach oddalonych o 30-40 centymetrów.'),
+ (5,2,'Warunki uprawy: pietruszkę naciową należy uprawiać na glebach lekkich, obojętnych i bogatych w próchnicę. ');
 INSERT INTO "rosliny" ("id_r","nazwa","odmiana","okres_siewu_poczatek","okres_siewu_koniec","okres_zbioru_poczatek","okres_zbioru_koniec","czestotliwosc_podlewania") VALUES (1,'Marchew','wczesna','2020-03-15 00:00:00','2020-04-15 00:00:00','2020-06-01 00:00:00','2020-07-30 00:00:00',3),
- (2,'Pietruszka','naciowa Mooskrause 2 - kÄ™dzieÅ¼awa','2020-03-01 00:00:00','2020-05-30 00:00:00','2020-05-30 00:00:00','2020-12-31 00:00:00',7),
- (3,'Burak','Cukrowy podÅ‚uÅ¼ny Opolski','2020-03-15 00:00:00','2020-04-15 00:00:00','2020-07-15 00:00:00','2020-09-30 00:00:00',14),
+ (2,'Pietruszka','naciowa Mooskrause 2 - kędzieżawa','2020-03-01 00:00:00','2020-05-30 00:00:00','2020-05-30 00:00:00','2020-12-31 00:00:00',7),
+ (3,'Burak','Cukrowy podłużny Opolski','2020-03-15 00:00:00','2020-04-15 00:00:00','2020-07-15 00:00:00','2020-09-30 00:00:00',14),
  (5,'Koper','ogrodowy Sprinter','2020-03-01 00:00:00','2020-04-30 00:00:00','2020-05-01 00:00:00','2020-10-30 00:00:00',1),
  (6,'Dynia','olbrzymia Uchiki Kuri','2020-05-01 00:00:00','2020-05-30 00:00:00','2020-08-01 00:00:00','2020-09-30 00:00:00',2);
+DROP TRIGGER IF EXISTS "dodaj_powiadomienie_status_z";
+CREATE TRIGGER `dodaj_powiadomienie_status_z` AFTER UPDATE ON egzemplarze FOR EACH ROW
+ WHEN NEW.status = 'Z'
+ BEGIN
+INSERT INTO powiadomienia(id_egzemplarza, id_ogrodka,tresc,data_powiadomienia) VALUES 
+(NEW.id_e,NEW.id_ogrodka,'Gotowe do zebrania!',(SELECT okres_zbioru_poczatek FROM rosliny WHERE rosliny.id_r = NEW.id_rosliny)); 
+ END;
+DROP TRIGGER IF EXISTS "dodaj_powiadomienie_status_nz";
+CREATE TRIGGER `dodaj_powiadomienie_status_nz` AFTER INSERT ON egzemplarze FOR EACH ROW
+ WHEN NEW.status = 'NZ'
+ BEGIN
+ INSERT INTO powiadomienia(id_egzemplarza,id_ogrodka,tresc,data_powiadomienia) VALUES
+ (NEW.id_e, NEW.id_ogrodka,'Nie zapomnij mnie zasadzić!',(SELECT okres_siewu_koniec FROM rosliny WHERE rosliny.id_r = NEW.id_rosliny)),
+ (NEW.id_e, NEW.id_ogrodka,'Możesz mnie zasadzić!',(SELECT okres_siewu_poczatek FROM rosliny WHERE rosliny.id_r = NEW.id_rosliny)); 
+ END;
 DROP TRIGGER IF EXISTS "dodaj_powiadomienie_nowy_ogrodek_kolejny_rok";
 CREATE TRIGGER `dodaj_powiadomienie_nowy_ogrodek_kolejny_rok` AFTER INSERT ON ogrodki FOR EACH ROW
  WHEN (datetime('now') not between datetime('now', 'start of year') and datetime('now', 'start of year','+3 months'))
@@ -142,21 +148,6 @@ CREATE TRIGGER `dodaj_powiadomienie_nowy_ogrodek` AFTER INSERT ON ogrodki FOR EA
  WHEN (datetime('now') between datetime('now', 'start of year') and datetime('now', 'start of year','+3 months'))
  BEGIN INSERT INTO powiadomienia(id_ogrodka,tresc,data_powiadomienia) VALUES 
 (NEW.id_og,'Przekop ogródek!',(SELECT datetime ('now','start of year','end of month','+2 month')));
- END;
-DROP TRIGGER IF EXISTS "dodaj_powiadomienie_status_z";
-CREATE TRIGGER dodaj_powiadomienie_status_z AFTER UPDATE ON egzemplarze FOR EACH ROW
- WHEN NEW.status = 'Z'
- BEGIN
-INSERT INTO powiadomienia(id_egzemplarza, id_ogrodka,tresc,data_powiadomienia) VALUES 
-(NEW.id_e,NEW.id_ogrodka,'Gotowe do zebrania!',(SELECT okres_zbioru_poczatek FROM rosliny WHERE rosliny.id_r = NEW.id_rosliny)); 
- END;
-DROP TRIGGER IF EXISTS "dodaj_powiadomienie_status_nz";
-CREATE TRIGGER dodaj_powiadomienie_status_nz AFTER UPDATE ON egzemplarze FOR EACH ROW
- WHEN NEW.status = 'NZ'
- BEGIN
- INSERT INTO powiadomienia(id_egzemplarza,id_ogrodka,tresc,data_powiadomienia) VALUES
- (NEW.id_e, NEW.id_ogrodka,'Nie zapomnij mnie zasadzić!',(SELECT okres_siewu_koniec FROM rosliny WHERE rosliny.id_r = NEW.id_rosliny)),
- (NEW.id_e, NEW.id_ogrodka,'Możesz mnie zasadzić!',(SELECT okres_siewu_poczatek FROM rosliny WHERE rosliny.id_r = NEW.id_rosliny)); 
  END;
 DROP TRIGGER IF EXISTS "zmien_powiadomienie_status_ze";
 CREATE TRIGGER zmien_powiadomienie_status_ze AFTER UPDATE ON egzemplarze FOR EACH ROW
